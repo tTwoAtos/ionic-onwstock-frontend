6ee6f7bc7fec7cb6efd6dd0e97dab7267d430e1f
Julien <julien.rebiffe@gmail.com>
2024-02-14 11:00:40 +0100

    Refactoring product UI + Pub UI

---
+++ src/app/tab2/productcard.page.ts
@@ -0,0 +1,18 @@
+export let productcards = [
+    {
+        alt: 'Silhouette of mountains',
+        src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
+        name: 'Name product',
+        quantity: 'Quantities',
+        category: 'Categories',
+        noLines: true
+    },
+    {
+        alt: 'Silhouette of mountains',
+        src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
+        name: 'Name product',
+        quantity: 'Quantities',
+        category: 'Categories',
+        noLines: false
+    }
+]
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { productcards } from './productcard.page';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
-
-  constructor() {}
-
+  constructor() { }
+  public cardsData = productcards
 }
