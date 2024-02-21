562d3532e4325effbff8fdea7a47ff090dacfdf5
hamzaRebb <s35394@stagiaire.aelion.fr>
2024-02-21 09:34:54 +0100

    Refactor productcard and pubcard data structures

--- src/app/tab2/static/productcard.page.ts
+++ src/app/tab2/static/productcard.page.ts
@@ -1,6 +1,6 @@
 import ProductCard from "../types/productcard.type"
 
-export let productcard: ProductCard[] = [
+export const productcard: ProductCard[] = [
     {
         alt: 'Eau de source - Cristaline - 1,5 L',
         src: 'https://images.openfoodfacts.org/images/products/327/408/000/5003/front_fr.950.400.jpg',
@@ -10,61 +10,6 @@ export let productcard: ProductCard[] = [
             'Boissons',
             ' Eaux',
             ' Eaux de sources'
-        ]
-    },
-    {
-        alt: 'Silhouette of mountains',
-        src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
-        name: 'Product Name',
-        quantity: 'Quantities',
-        category: [
-            'Subcategory 1',
-            ' Subcategory 2',
-            ' Subcategory 3'
-        ]
-    },
-    {
-        alt: 'Silhouette of mountains',
-        src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
-        name: 'Product Name',
-        quantity: 'Quantities',
-        category: [
-            'Subcategory 1',
-            ' Subcategory 2',
-            ' Subcategory 3'
-        ]
-    },
-    {
-        alt: 'Silhouette of mountains',
-        src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
-        name: 'Product Name',
-        quantity: 'Quantities',
-        category: [
-            'Subcategory 1',
-            ' Subcategory 2',
-            ' Subcategory 3'
-        ]
-    },
-    {
-        alt: 'Silhouette of mountains',
-        src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
-        name: 'Product Name',
-        quantity: 'Quantities',
-        category: [
-            'Subcategory 1',
-            ' Subcategory 2',
-            ' Subcategory 3'
-        ]
-    },
-    {
-        alt: 'Silhouette of mountains',
-        src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
-        name: 'Product Name',
-        quantity: 'Quantities',
-        category: [
-            'Subcategory 1',
-            ' Subcategory 2',
-            ' Subcategory 3'
         ]
     }
 ]
--- src/app/tab2/static/pubcard.page.ts
+++ src/app/tab2/static/pubcard.page.ts
@@ -1,16 +1,10 @@
 import PubCards from "../types/pubcards.type"
 
-export let pubcards: PubCards[] = [
+export const pubcards: PubCards[] = [
     {
         alt: 'Silhouette of mountains',
         src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
         name: 'Pub Name',
-        description: 'Pub Description'
-    },
-    {
-        alt: 'Silhouette of mountains',
-        src: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
-        name: 'Pub Name 2',
         description: 'Pub Description'
     }
 ]
