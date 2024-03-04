08b6bf8931a8c412fdb2d17705e4f24cfde3e5c2
hamzaRebb <s35394@stagiaire.aelion.fr>
2024-03-04 15:53:39 +0100

    Add ModalsModule and command for serving Ionic app

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -6,7 +6,7 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
 import { Tab2Page } from './tab2.page'
 
 import { PubPageComponent as PubPage } from './pub/pub.component'
-
+import { ModalsModule } from '../modals/modals'
 import { Tab2PageRoutingModule } from './tab2-routing.module'
 
 @NgModule({
@@ -16,6 +16,7 @@ import { Tab2PageRoutingModule } from './tab2-routing.module'
     FormsModule,
     ExploreContainerComponentModule,
     Tab2PageRoutingModule,
+    ModalsModule
   ],
 
   providers: [
