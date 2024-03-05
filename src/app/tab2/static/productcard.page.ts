920c9b05d9d22eaae9648baa8dcc452e99eb28a3
hamzaRebb <s35394@stagiaire.aelion.fr>
2024-03-05 16:12:48 +0100

    Fix product quantity buttons in tab2 page

--- src/app/tab2/static/productcard.page.ts
+++ src/app/tab2/static/productcard.page.ts
@@ -11,5 +11,16 @@ export const productcard: ProductCard[] = [
             ' Eaux',
             ' Eaux de sources'
         ]
+    },
+    {
+        alt: 'Eau de source - Cristaline - 1,5 L',
+        thumbnail: 'https://images.openfoodfacts.org/images/products/327/408/000/5003/front_fr.950.400.jpg',
+        name: 'Eau de source - Cristaline - 1,5 L',
+        quantity: 3,
+        category: [
+            'Boissons',
+            ' Eaux',
+            ' Eaux de sources'
+        ]
     }
 ]
