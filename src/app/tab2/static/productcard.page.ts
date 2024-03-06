6ab7a5a1060f385c32954737547c93df46977303
hamzaRebb <s35394@stagiaire.aelion.fr>
2024-03-06 15:23:56 +0100

    Fix localStorage bug in productcard.page.ts and tab2.page.ts

--- src/app/tab2/static/productcard.page.ts
+++ src/app/tab2/static/productcard.page.ts
@@ -1,6 +1,6 @@
 import ProductCard from "../types/productcard.type"
 
-export const productcard: ProductCard[] = JSON.parse(localStorage.getItem('productcard') || '[]') || [
+export const productcard: ProductCard[] = /*JSON.parse(localStorage.getItem('productcard') || '[]') ||*/ [
     {
         alt: 'Eau de source - Cristaline - 1,5 L',
         thumbnail: 'https://images.openfoodfacts.org/images/products/327/408/000/5003/front_fr.950.400.jpg',
