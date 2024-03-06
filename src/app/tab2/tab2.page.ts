9ae1082f71d58048574610c6a02601b4cf996a6a
hamzaRebb <s35394@stagiaire.aelion.fr>
2024-03-06 13:37:48 +0100

    Update pub name and medium color

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -63,11 +63,17 @@ export class Tab2Page {
     const removeBtn: HTMLIonFabButtonElement | null = document.getElementById(`removeQte${index}`) as HTMLIonFabButtonElement
     if (product.quantity === 0) removeBtn.disabled = true
     // TODO: call service to update the quantity of the product
+    /*this.productService.updateProduct(product, productToCommunityID).subscribe(datas => {
+      this.product = datas
+    })*/
   }
 
   addQuantity(product: ProductCard, index: number) {
     product.quantity++
     // TODO: call service to increase the quantity of the product
+    /*this.productService.updateProduct(product, productToCommunityID).subscribe(datas => {
+      this.product = datas
+    })*/
     const removeBtn: HTMLIonFabButtonElement | null = document.getElementById(`removeQte${index}`) as HTMLIonFabButtonElement
     removeBtn.disabled = false
   }
